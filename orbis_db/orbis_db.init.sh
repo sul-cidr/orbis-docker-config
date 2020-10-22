5e85ad35e82081ef36f5ae5b39bb2687857b8709
Simon Wiles <simonjwiles@gmail.com>
2020-10-22 10:21:33 -0700

    Add more users needed to restore db dump

--- orbis_db/orbis_db.init.sh
+++ orbis_db/orbis_db.init.sh
@@ -3,6 +3,11 @@
 if [[ -n $(su --login postgres --command "psql -d orbis -c '\q' 2>&1") ]];
 then
   su --login postgres --command "createdb orbis";
+  su --login postgres --command "createuser --superuser power";
+  su --login postgres --command "createuser --superuser elijah";
+  su --login postgres --command "createuser --superuser karlg";
+  su --login postgres --command "createuser --superuser orbis";
+  su --login postgres --command "createuser --superuser kb";
   su --login postgres --command "createuser --superuser webapp";
 
   cp /opt/lib/pgrouting/librouting* /usr/lib/postgresql/9.1/lib/;
